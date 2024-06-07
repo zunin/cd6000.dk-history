82cc415677a8a9baaa8c316efb558b2670aa6968
Nikolai Øllegaard <nikolai@oellegaard.dk>
2024-06-07 20:27:59 +0200

    feat: prettify formatting for cds.json

--- index.ts
+++ index.ts
@@ -69,4 +69,4 @@ const artistLookup = lookupList.reduce((previousValue, currentValue) => {
     }
 }, {} as AlbumLookupByArtist);
 
-Deno.writeTextFile("./cds.json", JSON.stringify(artistLookup))
+Deno.writeTextFile("./cds.json", JSON.stringify(artistLookup, null, '\t'));
